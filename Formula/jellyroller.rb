8758c1c145ebaec3f82d8d22428fce126aaeabdf
LSchallot <l.schallot@gmail.com>
2025-01-12 15:57:38 -0600

    Update jellyroller.rb

--- Formula/jellyroller.rb
+++ Formula/jellyroller.rb
@@ -1,8 +1,8 @@
 class Jellyroller < Formula
   desc "CLI Jellyfin Controller Utility for Linux and Windows"
   homepage ""
-  url "https://github.com/LSchallot/JellyRoller/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "4b73fdd692c07cd698026fcaf3b53b0234afe707799f1afd67786e1efcde25b7"
+  url "https://github.com/LSchallot/JellyRoller/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "3a3b47b98260cb76fb6976ceab2ab77f88c94fd415bdceda0dffa24d1be9f72a"
   license "GPL-2.0"
   
   depends_on "rust" => :build
